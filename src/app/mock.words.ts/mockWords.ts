588015d0a7b48f8b7f80b5ee5fb3c2b35d51897d
evanzs <evandrofidk@hotmail.com>
2022-12-31 20:31:28 -0300

    feat(mockwords)::sparkles: create words

---
+++ src/app/mock.words.ts/mockWords.ts
@@ -0,0 +1,76 @@
+import Word from "../interface/word";
+
+export const WORDS:Array<Word> = [
+
+    { name: "Sim", response: true },
+    { name: "Claro", response: true },
+    { name: "Talvez", response: true },
+    { name: "Mais Tarde", response: false },
+    { name: "Jamais!", response: false },
+   
+    { name: "Mestra da Terra Plana", response: false },
+    { name: "Mestra dos Magos", response: false },
+    { name: "Caveleira", response: false },
+    { name: "Maga", response: false },
+    { name: "Feiticeira", response: false },
+
+    { name: "Continuar", response: true },
+    { name: "Seguir", response: true },
+    { name: "Vamo!", response: true },
+    { name: "Ainda nao foi?", response: true },
+    { name: "VAI!", response: true },
+   
+    { name: "MDS CARA!", response: true },
+    { name: "????", response: true },
+    { name: "Enrolado!", response: true },
+    { name: "Enrolação!", response: true },
+    { name: "¿¿¿¿", response: true },
+
+    { name: "No altar", response: false },
+    { name: "Na politica", response: false },
+    { name: "No banheiro", response: true },
+    { name: "Na escola", response: false },
+    { name: "Russia!", response: false },
+
+    { name: "Amor", response: true },
+    { name: "Porrete", response: false },
+    { name: "Molho Vermelho", response: false },
+    { name: "Garfo", response: false },
+    { name: "Faca", response: false },
+
+    { name: "Diversão", response: false },
+    { name: "Chaveiro", response: false },
+    { name: "Companherismo", response: false },
+    { name: "Casaco", response: false },
+    { name: "Mesa", response: true },
+
+
+    { name: "Buraco", response: false },
+    { name: "Casal", response: false },
+    { name: "Ovo", response: true },
+    { name: "Detalhe", response: false },
+    { name: "Moderno", response: false },
+
+    { name: "Buraco", response: false },
+    { name: "Casal", response: false },
+    { name: "Ovo", response: true },
+    { name: "Detalhe", response: false },
+    { name: "Moderno", response: false },
+
+    { name: "Pochete", response: false },
+    { name: "Respeito", response: true },
+    { name: "Balsa", response: true },
+    { name: "Surpresas", response: false },
+    { name: "Facão", response: false },
+
+    { name: "A Tesoura", response: true },
+    { name: "Faca", response: true },
+    { name: "Cordão", response: false },
+    { name: "Palavras", response: false },
+    { name: "Amor", response: false },
+
+
+
+
+
+]
